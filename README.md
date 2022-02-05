4828d6c95b3295db033b48bc048158526a689787
J-H-Chandler <j.h.chandler@leeds.ac.uk>
2022-02-05 13:59:43 +0000

    Hello-World!

---
+++ README.md
@@ -0,0 +1,10 @@
+Hello-World-Template
+  ===========
+  
+
+*Template from which serial output commands can be added.
+         
+  MBED Studio version: 1.4.1 
+  MBED OS version: 6.8.0
+  Board: NUCLEO L476RG 
+  Author: James Chandler 2021
---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*  Hello-World-Template
+*   This template code is a base from which serial output commands can be added.
+*   
+*   The code should be modified in accordance with the Seial Communication Activities. 
+*
+*   Board: NUCLEO L476RG
+*   Author: James Chandler 2021
+*/
+#include "mbed.h"
+
+DigitalOut led1(PC_0);
+
+int main()
+{
+    led1.write(1); // The LED is set high for 1 sec and then switched off to help identify code is active/reset
+    // Add code here to print a message to the user. 
+    thread_sleep_for(1000);
+    led1.write(0);
+}
